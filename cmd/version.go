10129c092c5d0977dd559c4a4ff698043554d89b
Alex Stan <alexstan@Alexs-MBP.broadband>
2024-07-22 12:50:04 +0100

    Added version command and improved readme

---
+++ cmd/version.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2024 Alex Stan
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "v0.0.1"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Command for showing the version for multi-k8s.",
+	Long:  `Use this command to get the version for multi-k8s.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("multi-k8s version: %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}

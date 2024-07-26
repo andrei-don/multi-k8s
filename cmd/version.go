4ff4797af53ecf8220926433e366cf7426361a34
Alex Stan <alexstan@Alexs-MBP.broadband>
2024-07-26 12:56:49 +0100

    Added ldflags for replacing the version at build time

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "v0.0.2"
+var Version = "replaced_by_goreleaser"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
